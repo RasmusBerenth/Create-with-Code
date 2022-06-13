fcf17e2d25da06eee2ba0687230a45db6c3d8aa7
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2022-06-13 11:40:47 +0200

    finished challenge 2

--- Prototyp 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Prototyp 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public float spawnDuration = 3.0f;
+    private float spawnTime;
 
+    private void Start()
+    {
+        spawnTime = spawnDuration;
+    }
     // Update is called once per frame
     void Update()
     {
+        bool canSpawnDog = spawnTime >= spawnDuration;
+        spawnTime += Time.deltaTime;
+
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && canSpawnDog)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            spawnTime = 0;
         }
     }
 }
